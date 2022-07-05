34fca3b66a9b2f19239f0264332ebfbd43d5adc8
Lee <leven.cn@gmail.com>
2022-07-05 23:57:35 +0800

    add test case for `attrs` decorator. (#28)

---
+++ tests/test_decorators.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+import pytest
+
+from src.handy.decorators import attrs
+
+
+class TestDecorators:
+    @pytest.fixture
+    def attrs_func_no_argument(self):
+        @attrs(any_attr_1=1, any_attr_2='2')
+        def func():
+            '''attrs_func for testing.'''
+            return 9
+
+        return func
+
+    @pytest.fixture
+    def attrs_func_with_arguments(self):
+        @attrs(any_attr_1=3, any_attr_2='4')
+        def func_arguments(*args: Any):
+            '''attrs_func for testing.'''
+            return 9 + args[0]
+
+        return func_arguments
+
+    def test_attrs(
+        self,
+        attrs_func_no_argument: Any,
+        attrs_func_with_arguments: Any,
+    ):
+        assert attrs_func_no_argument.__name__ == 'func'
+        assert attrs_func_no_argument.__doc__ == 'attrs_func for testing.'
+        assert attrs_func_no_argument.any_attr_1 == 1
+        assert attrs_func_no_argument.any_attr_2 == '2'
+        assert attrs_func_no_argument() == 9
+
+        assert attrs_func_with_arguments.__name__ == 'func_arguments'
+        assert attrs_func_with_arguments.__doc__ == 'attrs_func for testing.'
+        assert attrs_func_with_arguments.any_attr_1 == 3
+        assert attrs_func_with_arguments.any_attr_2 == '4'
+        assert attrs_func_with_arguments(1) == 9 + 1
